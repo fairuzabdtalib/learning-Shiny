352bdae3173170a473897d03adbde3ae57744420
fairuz_work <fairuzat@singlebuyer.com.my>
2024-10-01 15:56:19 +0800

    capitalise all

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app_ui = ui.page_navbar(
             ui.sidebar(
                 
                 ui.input_file("sales_csv", "Upload Actual Sales (csv)", accept=[".csv"]),
-                ui.input_date_range("date_sales", "Model training period", start="2020-09-01", end="2024-06-30", format="M yyyy"),
-                ui.input_numeric("sales_forecast_horizon", "Forecast horizon", value=6),
-                ui.input_action_button("run_sales_model", "Run model"),
-                ui.input_action_button("export_sales_forecast", "Export forecast")
+                ui.input_date_range("date_sales", "Model Training Period", start="2020-09-01", end="2024-06-30", format="M yyyy"),
+                ui.input_numeric("sales_forecast_horizon", "Forecast Horizon", value=6),
+                ui.input_action_button("run_sales_model", "Run Model"),
+                ui.input_action_button("export_sales_forecast", "Export Forecast")
                 
             ),
             
@@ -20,10 +20,10 @@ app_ui = ui.page_navbar(
             ui.sidebar(
                 
                 ui.input_file("generation_csv", "Upload Actual Gen (csv)", accept=[".csv"]),
-                ui.input_date_range("date_generation", "Model training period", start="1978-01-01", end="2024-06-30", format="M yyyy"),
-                ui.input_numeric("generation_forecast_horizon", "Forecast horizon", value=6),
-                ui.input_action_button("run_generation_model", "Run model"),
-                ui.input_action_button("export_generation_forecast", "Export forecast")
+                ui.input_date_range("date_generation", "Model Training Period", start="1978-01-01", end="2024-06-30", format="M yyyy"),
+                ui.input_numeric("generation_forecast_horizon", "Forecast Horizon", value=6),
+                ui.input_action_button("run_generation_model", "Run Model"),
+                ui.input_action_button("export_generation_forecast", "Export Forecast")
                 
             ),
             
